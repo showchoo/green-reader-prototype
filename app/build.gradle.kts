967d9261950b6a25f0fbc3d5a7882e1a77e52a78
showchoo <34552117+showchoo@users.noreply.github.com>
2026-09-16 20:35:52 +0900

    v0.7.1 に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "jp.example.greenreader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.7.0"
+        versionCode = 11
+        versionName = "0.7.1"
     }
 
     compileOptions {
