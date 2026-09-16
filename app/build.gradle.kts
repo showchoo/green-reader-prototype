8092df065a8cf443747d5c90c4710b39ff68a2da
showchoo <34552117+showchoo@users.noreply.github.com>
2026-09-16 19:17:59 +0900

    Bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "jp.example.greenreader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
     }
 
     compileOptions {
