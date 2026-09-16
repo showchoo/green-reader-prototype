4631684492d9abc90d2a300cf0146ec8875422f9
showchoo <34552117+showchoo@users.noreply.github.com>
2026-09-16 21:44:00 +0900

    タイトル画面を省略して直接スキャンを開始 v0.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "jp.example.greenreader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.7.6"
+        versionCode = 17
+        versionName = "0.7.7"
     }
 
     compileOptions {
